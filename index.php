bbf86a11f3d914e75858b7e128239d0b621fbc35
valentina891 <valemil.vs@gmail.com>
2020-09-29 17:03:34 +0200

    inizio esercizio CRUD

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="dist/app.css">
+        <title>Php CRUD</title>
+    </head>
+    <body>
+
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Numero stanza</th>
+                        <th>Floor</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><a href="#">VIEW</a></td>
+                        <td><a href="#">UPDATE</a></td>
+                        <td><a href="#">DELETE</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
