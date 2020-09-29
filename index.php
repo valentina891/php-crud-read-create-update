e3988ca11416568c4a382a21042287e7b2ab9527
valentina891 <valemil.vs@gmail.com>
2020-09-29 18:03:40 +0200

    push - tabella con risultati

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="dist/app.css">
-        <title>Php CRUD</title>
-    </head>
+<?php
+include __DIR__ .'/partials/template/header.php';
+include __DIR__ .'/partials/home/server.php';
+?>
     <body>
 
         <div class="container">
@@ -20,14 +17,16 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php foreach ($results as $room) { ?>
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><?php echo $room['id'];?></td>
+                        <td><?php echo $room['room_number'];?></td>
+                        <td><?php echo $room['floor'];?></td>
                         <td><a href="#">VIEW</a></td>
                         <td><a href="#">UPDATE</a></td>
                         <td><a href="#">DELETE</a></td>
                     </tr>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
